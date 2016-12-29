c59bec9be559d789b21e218c4dfad38160c021cb
Seth Itow <seth@Seths-MacBook-Pro.local>
2016-12-29 02:13:48 -0800

    Added support for ATtiny Build Target

--- irReceiver/config.h
+++ irReceiver/config.h
@@ -1,4 +1,20 @@
+//Uncomment the desired build target
+//#define ATTINY_BUILD_TARGET
+#define ARDUINO_UNO_BUILD_TARGET
+
+#define SERIAL_DEBUG //Uncomment to enable serial debugging
+
+
+
+#ifdef ARDUINO_UNO_BUILD_TARGET
 #define IN_PIN 2 // The digital pin to which the IR receiver is attached. Must support ISR
+#define OUT_PIN 3 // The digital pin to output signal
+#endif
+
+#ifdef ATTINY_BUILD_TARGET
+#define IN_PIN 4 // The digital pin to which the IR receiver is attached. Must support ISR
+#define OUT_PIN 3 // The digital pin to output signal
+#endif
 
 #define LONG_PULSE 5900 //length of the demodulated long pulse in microseconds
 #define SHORT_PULSE 1190 // length of the demodulated short pulse in microseconds
@@ -7,7 +23,7 @@
 
 #define TOKEN_TARGET 3
 
-#define IGNORE_AFTER_LAP_DURATION 5000
+#define IGNORE_AFTER_LAP_DURATION 1000
 
 
 //Don't change anything below here
--- irReceiver/irReceiver.ino
+++ irReceiver/irReceiver.ino
@@ -13,19 +13,23 @@ void IRInterrupt(void) {
 
   isRisingEdge = digitalRead(IN_PIN);
 
-  if(isRisingEdge){
+  if (isRisingEdge) {
     startEdge = micros();
   }
   else {
     pulseTime = micros() - startEdge;
     pulseCompleted = true;
   }
-    
+
 }
 
 void setup() {
+
+#ifdef SERIAL_DEBUG
   Serial.begin(9600);
   Serial.println("Ready...");
+#endif
+
 
   pulseCompleted = true;
   isRisingEdge = false;
@@ -33,28 +37,48 @@ void setup() {
   longPulses = 0;
   shortPulses = 0;
 
-  pinMode(IN_PIN,INPUT);
-  attachInterrupt(digitalPinToInterrupt(IN_PIN), IRInterrupt, CHANGE);
+  pinMode(IN_PIN, INPUT);
+  pinMode(OUT_PIN, OUTPUT);
 
+  digitalWrite(OUT_PIN, HIGH);
+  digitalWrite(OUT_PIN, LOW);
+
+//the following sketchyness is done to maintain best practice while using an ATMega based board
+
+#ifdef ATTINY_BUILD_TARGET
+  attachInterrupt(IN_PIN, IRInterrupt, CHANGE);
+#endif
+
+#ifdef ARDUINO_UNO_BUILD_TARGET
+  attachInterrupt(digitalPinToInterrupt(IN_PIN), IRInterrupt, CHANGE);
+#endif
 }
 
 void loop() {
-  
-  if (pulseCompleted){
+
+  if (pulseCompleted) {
     pulseCompleted = false;
     if (pulseTime > SHORT_MIN_PULSE && pulseTime < SHORT_MAX_PULSE) shortPulses++;
     else if (pulseTime > LONG_MIN_PULSE && pulseTime < LONG_MAX_PULSE) longPulses++;
-    
+
   }
 
   if (longPulses >= TOKEN_TARGET) {
-    int baseID = round(shortPulses/longPulses);
+    int baseID = round(shortPulses / longPulses);
     shortPulses = 0;
     longPulses = 0;
+
+#ifdef SERIAL_DEBUG
     Serial.print("Beacon "); Serial.print(baseID); Serial.println(" Detected");
+#endif
+
+    digitalWrite(OUT_PIN, HIGH);
+
     delay(IGNORE_AFTER_LAP_DURATION);
+
+    digitalWrite(OUT_PIN, LOW);
   }
-  else{
+  else {
     //Serial.println("No Beacon Was Detected, Oh No");
   }
 
